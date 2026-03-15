1396f43b1a1320d5305eb7d62ede73a681596ce0
yanxuan17 <420722471@qq.com>
2026-03-15 16:47:11 +0800

    初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'kimi-plugin-inspect-react'
+
+export default defineConfig({
+  base: './',
+  plugins: [inspectAttr(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    headers: {
+      'Content-Security-Policy': "script-src 'self' 'unsafe-eval' 'unsafe-inline';"
+    }
+  }
+});
